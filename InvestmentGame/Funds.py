3ba5479d7b0188680a20fa913f344b90d1a8202d
pengbersen <55739361+pengbersen@users.noreply.github.com>
2019-09-24 14:01:42 +0200

    defined class: Funds

---
+++ InvestmentGame/Funds.py
@@ -0,0 +1,9 @@
+class Fund:
+    def __init__(self,fundid,symbol,market,currency,name,price):
+        self.fundid = fundid
+        self.symbol = symbol
+        self.market = market
+        self.currency = currency
+        self.name = name
+        self.price = price
+
