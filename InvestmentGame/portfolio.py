73ff4f1747b51c02a74661e388d99a2278b30a4d
NTimmerhuis <ntimmerhuis@deloitte.nl>
2019-09-24 14:11:18 +0200

    Portfolioooo in plaats van Portfoli

--- InvestmentGame/portfolio.py
+++ InvestmentGame/portfolio.py
@@ -1,6 +1,6 @@
 class Portfolio:
     def __init__(self,portfolio_id, fund, name, aantal, value):
-        self.portfoli_id = portfolio_id
+        self.portfolio_id = portfolio_id
         self.fund = fund
         self.aantal = aantal
         self.value = value
