82c5c6f7427ea4616fd1242abcee0482c501e595
NTimmerhuis <ntimmerhuis@deloitte.nl>
2019-09-24 14:12:16 +0200

    name toegevoegd.

--- InvestmentGame/portfolio.py
+++ InvestmentGame/portfolio.py
@@ -2,6 +2,7 @@ class Portfolio:
     def __init__(self,portfolio_id, fund, name, aantal, value):
         self.portfolio_id = portfolio_id
         self.fund = fund
+        self.name = name
         self.aantal = aantal
         self.value = value
 
