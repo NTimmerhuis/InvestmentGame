971dd966175cdaa4628567ca13a33ed140100f25
RutgerJJ <rutger.jansen@hotmail.com>
2019-09-24 17:57:37 +0200

    merge

--- InvestmentGame.py
+++ InvestmentGame.py
@@ -1,20 +1,22 @@
-import requests
-import pandas as pd
+from InvestmentGame.order import Order
+order_nr = 1
+ordertype = ""
+order = input("You want to make an order? (y/n)")
+if order == "y":
+    while ordertype not in ("buy","sell"):
+        ordertype = input("buy or sell order?")
+        if ordertype == "buy":
+            newbuyorder = input("Time, price, amount, fund_name, status")
+            newbuyorder= newbuyorder.split(",")
+            newbuyorder.append(ordertype)
+            order_nr = Order(newbuyorder)
+            order_nr += 1
+        elif ordertype == "sell":
+            newsellorder = input("ID, price, amount, fund_name")
+        else:
+            print ("This is not an valid ordertype")
+elif order == "n":
+    print("Maybe next time")
+else:
+    print("This is not a valid answer")
 
-# importing data
-
-response = requests.get("https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&outputsize=full&apikey=demo")
-
-# Since we are retrieving stuff from a web service, it's a good idea to check for the return status code
-# See: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
-if response.status_code != 200:
-    raise ValueError("Could not retrieve data, code:", response.status_code)
-
-# The service sends JSON data, we parse that into a Python datastructure
-raw_data = response.json()
-
-# Let's look at the raw data (it's a lot so let's limit it)
-raw_data.keys()
-
-# This we probably don't need
-raw_data['Meta Data']
--- InvestmentGame/order.py
+++ InvestmentGame/order.py
@@ -1,12 +1,14 @@
 class Order:
-    def __init__(self, order_id, time, price, amount, order_type, fund_name, status):
-        self.order_id = order_id
+    order_id = 0
+    def __init__(self, time, price, amount, fund_name, status, order_type):
+        self.order_id = Order.order_id
         self.time = time
         self.price = price
         self.amount = amount
-        self.order_type = order_type
         self.fund_name = fund_name
         self.status = status
+        self.order_type = order_type
+        Order.order_id += 1
 
     def total_order_value(self):
         totalordervalue = self.amount * self.price
