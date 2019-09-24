83b8e5edeb945589dc9119e0160a40e08e715d81
pengbersen <55739361+pengbersen@users.noreply.github.com>
2019-09-24 14:04:58 +0200

    added portfolio

---
+++ InvestmentGame/portfolio.py
@@ -0,0 +1,7 @@
+class Portfolio:
+    def __init__(self,portfolioid,fund,name,aantal,value):
+        self.portfolioid = portfolioid
+        self.fund = fund
+        self.aantal = aantal
+        self.value = value
+        
