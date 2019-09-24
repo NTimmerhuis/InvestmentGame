19129054bfe90d5714a841381ea86bd9cb78c332
NTimmerhuis <ntimmerhuis@deloitte.nl>
2019-09-24 14:20:07 +0200

    nog een aanpassing om te kijken of ik de bestandsnaam kan wijzigen

--- InvestmentGame/portfolio.py
+++ InvestmentGame/portfolio.py
@@ -1,8 +1,8 @@
 class Portfolio:
-    def __init__(self,portfolio_id, fund, name, aantal, value):
+    def __init__(self,portfolio_id, fund, portfolio_name, aantal, value):
         self.portfolio_id = portfolio_id
         self.fund = fund
-        self.name = name
+        self.portfolio_name = portfolio_name
         self.aantal = aantal
         self.value = value
 
