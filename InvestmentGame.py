817905a2e1d4357150d1c73b6c1b156677210b65
NTimmerhuis <ntimmerhuis@deloitte.nl>
2019-09-25 11:59:57 +0200

    Merged.

--- InvestmentGame.py
+++ InvestmentGame.py
@@ -1,8 +1,8 @@
 #Hoofdcode voor de InvestmentGame, vanuit deze code worden alle andere codes aangeroepen.
 
-<<<<<<< HEAD
 from InvestmentGame.order import Order
-from InvestmentGame.User import User
+from InvestmentGame.User  import User
+from InvestmentGame.retrieve_data import get_price
 
 #Aanroepen van de user
 #Wat nog kan worden toegevoegd is een echte database waar alle users in worden opgeslagen.
@@ -22,29 +22,7 @@ elif user == "n":
     users.append(new_user)
 else:
     print("Invalid input, please check your input.")
-=======
 
-
-#
-# user = input("Do you have a user_id? (Yes/No)")
-# if user == "Yes":
-#     user_id = input("Please provide your user_id.")
-#     #Nu moeten we gaan zoeken in user.py en de informatie over de juiste user ophalen.
-# elif user == "No":
-#     user_name = input("What is your name?")
-#     currency = input("What is your preferred currency?")
-#     balance = input("What is your starting balance?")
-#     #Bovenstaande willen we toevoegen aan class users en nieuwe user_id maken, eerst volgende user1, user 2 etc.
-# else:
-#     print("Invalid input, please check your input.")
->>>>>>> master
-
-<<<<<<< HEAD
-=======
-from InvestmentGame.order import Order
-from InvestmentGame.User  import User
-from InvestmentGame.retrieve_data import get_price
->>>>>>> master
 
 ordertype = ""
 order = input("You want to make an order? (y/n)")
@@ -52,24 +30,13 @@ if order == "y":
     while order not in ("buy", "sell"):
         ordertype = input("buy or sell order?")
         if ordertype == "buy":
-<<<<<<< HEAD
-            newbuyorder = input("order_id, Time, price, amount, fund_name, status")
-            newbuyorder = newbuyorder.split(',')
-            newbuyorder.append(ordertype)
-            order = Order(*newbuyorder)
-=======
             neworder = input("order_id, Time, amount, symbol, status")
             neworder= neworder.split(',')
             neworder.append(ordertype)
             price = get_price(neworder[3], neworder[1])
             neworder.append(price)
             order = Order(*neworder)
-<<<<<<< HEAD
->>>>>>> master
             User.order.append(order)
-=======
-            User.orders.append(order)
->>>>>>> master
         elif ordertype == "sell":
             neworder = input("order_id, Time, amount, symbol, status")
             neworder = neworder.split(',')
