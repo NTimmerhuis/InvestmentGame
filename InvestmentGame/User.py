580c9db6f394cdad7f1217fd3b2c79d834a45c74
NTimmerhuis <ntimmerhuis@deloitte.nl>
2019-09-25 12:01:24 +0200

    user aangepast.

--- InvestmentGame/User.py
+++ InvestmentGame/User.py
@@ -2,15 +2,5 @@ class User:
     def _init_(self, user_name, currency, balance):
         self.user_name = user_name
         self.currency = currency
-<<<<<<< HEAD
-<<<<<<< HEAD
         self.balance = balance
         self.orders = []
-=======
-        self.balance = balanceelf.orders=[]
-=======
-        self.balance = balance
-        self.orders = []
->>>>>>> master
-
->>>>>>> master
