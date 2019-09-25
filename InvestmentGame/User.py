dfa5f3cf36f8972600d1871da5b1cfc9a74bb84a
NTimmerhuis <ntimmerhuis@deloitte.nl>
2019-09-25 09:49:34 +0200

    removed changes.

--- InvestmentGame/User.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def _init_(self, user_id, user_name, currency, balance):
-        self.user_id = user_id
-        self.user_name = user_name
-        self.currency = currency
-        self.balance = balance
